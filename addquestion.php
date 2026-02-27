8a19c382e2e8546c2dff079a8535d9ce0055289a
Gaurishankar Vhadle <vhadlegaurishankar@gmail.com>
2026-02-27 21:52:19 +0530

    Update addquestion.php

--- addquestion.php
+++ addquestion.php
@@ -14,9 +14,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
- 
-  
-    // Insert the new question into the questions table
     $questionText = $data->questionText;
     $sql = "INSERT INTO questions (question_text) VALUES (?)";
     $stmt = $conn->prepare($sql);
@@ -25,8 +22,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     if ($stmt->execute()) {
         $questionId = $stmt->insert_id;
         $stmt->close();
-
-        // Insert options and their correctness into the options table
         for ($i = 1; $i <= 4; $i++) {
             $optionText = $data->{"option$i"};
             $isCorrect = ($i == $data->correctOption) ? 1 : 0;
@@ -36,11 +31,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             $stmt = $conn->prepare($sql);
             $stmt->bind_param("isi", $questionId, $optionText, $isCorrect);
             $stmt->execute();
-
-
-
-        }
-                    
+        }               
         $sql = "INSERT INTO test_questions (test_id, question_id) VALUES (?, ?)";
         $stmt = $conn->prepare($sql);
         $stmt->bind_param("ii", $testId, $questionId);
