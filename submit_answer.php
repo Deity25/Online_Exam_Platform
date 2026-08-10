f5c1f762e1f03d5d79e5586a44b1058a32556221
Gaurishankar Vhadle <vhadlegaurishankar@gmail.com>
2026-08-10 22:13:24 +0530

    Update submit_answer.php

--- submit_answer.php
+++ submit_answer.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "exam_db"; // Change this to your database name
+$dbname = "exam_db"; 
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
