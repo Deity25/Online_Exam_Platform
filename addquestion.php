6f0e987e81f0b629e9edc8abb500be58b4382aee
Gaurishankar Vhadle <vhadlegaurishankar@gmail.com>
2026-09-03 22:25:36 +0530

    Update addquestion.php

--- addquestion.php
+++ addquestion.php
@@ -9,7 +9,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     // Establish a database connection
     $conn = new mysqli($servername, $username, $password, $dbname);
-
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
