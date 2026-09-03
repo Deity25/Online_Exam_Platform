1dadced87a8a7e691f370e374a859a6ea3f904bc
Gaurishankar Vhadle <vhadlegaurishankar@gmail.com>
2026-09-03 22:25:30 +0530

    Update addquestion.php

--- addquestion.php
+++ addquestion.php
@@ -1,7 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $data = json_decode(file_get_contents("php://input"));
-
     // Database configuration
     $servername = "localhost";
     $username = "root";
